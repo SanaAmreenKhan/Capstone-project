fd257a05a76d2bd7627b0cfa3824616545fff228
SanaAmreenKhan <sanaamreenkhan9156@gmail.com>
2026-05-04 17:58:24 +0530

    imp term

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
-        'PASSWORD': 'Engineer#9156',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
